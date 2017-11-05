a3fc3e657c4defcdc29ddccf9ef17f7903d22fa2
user <muhammed.awad@outlook.com>
2017-11-05 12:25:41 +0200

    finall project

--- app/src/main/java/abd/askandr/mhmd/finallhayamuhammad/LogInActivity.java
+++ app/src/main/java/abd/askandr/mhmd/finallhayamuhammad/LogInActivity.java
@@ -1,13 +1,24 @@
 package abd.askandr.mhmd.finallhayamuhammad;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogInActivity extends AppCompatActivity {
     private EditText etName, etPassword;
     private Button btnLogin, btnSignup, btnForget;
+    private FirebaseAuth auth;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +29,33 @@ public class LogInActivity extends AppCompatActivity {
         btnLogin= (Button) findViewById(R.id.btnLogin);
         btnSignup = (Button) findViewById(R.id.btnSignup);
         btnForget = (Button) findViewById(R.id.btnForget);
+        auth=FirebaseAuth.getInstance();
+        firebaseUser=auth.getCurrentUser();
+
+
     }
-}
+    private  void dataHandler() {
+        String stName = etName.getText().toString();
+        String stPass = etPassword.getText().toString();
+        signIn(stName,stPass);
+
+    }
+
+    private void signIn(String email, String passw) {
+        auth.signInWithEmailAndPassword(email,passw).addOnCompleteListener(LogInActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(LogInActivity.this, "signIn Successful.", Toast.LENGTH_SHORT).show();
+                    Intent intent=new Intent(LogInActivity.this,MainListActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(LogInActivity.this, "signIn failed."+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+                }
+    }
+    }
--- app/src/main/java/abd/askandr/mhmd/finallhayamuhammad/SignupActivity.java
+++ app/src/main/java/abd/askandr/mhmd/finallhayamuhammad/SignupActivity.java
@@ -5,11 +5,16 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SignupActivity extends AppCompatActivity
 {
 
     private EditText etName, etPass,etRePass, etMail;
     private Button btnSave;
+    private FirebaseAuth auth;
+    private FirebaseUser firebaseUser;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -20,6 +25,27 @@ public class SignupActivity extends AppCompatActivity
         etRePass = (EditText) findViewById(R.id.etRePass);
         etMail = (EditText) findViewById(R.id.etMail);
         btnSave = (Button) findViewById(R.id.btnSave);
+        auth = FirebaseAuth.getInstance();
+        firebaseUser = auth.getCurrentUser();
+
+
     }
 
-}
+    private void dataHandler() {
+        String stemail = etMail.getText().toString();
+        String stpassword = etPass.getText().toString();
+        String stname = etName.getText().toString();
+        String phone = etRePass.getText().toString();
+        boolean isOk = true;
+        if (stemail.length() == 0 || stemail.indexOf('@') < 1) {
+            etMail.setError("WRONG EMAIL");
+            isOk = false;
+        }
+        if (stpassword.length() < 8) {
+            etPass.setError("BAD PASSWORD");
+            isOk = false;
+        }
+        if (isOk) {
+            creatAcount(stemail, stpassword);
+
+    }
