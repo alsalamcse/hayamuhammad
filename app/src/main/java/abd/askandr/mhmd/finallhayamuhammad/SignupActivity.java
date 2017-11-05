88f219a2d6e48c1d4570159895fedbaa1f9951e3
user <muhammed.awad@outlook.com>
2017-11-05 13:22:06 +0200

    finall project

--- app/src/main/java/abd/askandr/mhmd/finallhayamuhammad/SignupActivity.java
+++ app/src/main/java/abd/askandr/mhmd/finallhayamuhammad/SignupActivity.java
@@ -1,14 +1,20 @@
 package abd.askandr.mhmd.finallhayamuhammad;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class SignupActivity extends AppCompatActivity
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener
 {
 
     private EditText etName, etPass,etRePass, etMail;
@@ -27,6 +33,12 @@ public class SignupActivity extends AppCompatActivity
         btnSave = (Button) findViewById(R.id.btnSave);
         auth = FirebaseAuth.getInstance();
         firebaseUser = auth.getCurrentUser();
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dataHandler();
+            }
+        });
 
 
     }
@@ -47,5 +59,26 @@ public class SignupActivity extends AppCompatActivity
         }
         if (isOk) {
             creatAcount(stemail, stpassword);
+
+        }
+    }
+
+    private void creatAcount(String email, String passw) {
+        auth.createUserWithEmailAndPassword(email, passw).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(SignupActivity.this, "Authentication Successful.", Toast.LENGTH_SHORT).show();
+                    finish();
+                } else {
+                    Toast.makeText(SignupActivity.this, "Authentication Failed." + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+                }
+            }
+        });
+        FirebaseAuth.getInstance().signOut();
+    }
+
+    {
 
     }
