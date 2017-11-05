4a04b3b397ec38bc825f76be047347d7c19574d0
user <muhammed.awad@outlook.com>
2017-11-05 11:33:59 +0200

    finall project

--- app/src/main/java/abd/askandr/mhmd/finallhayamuhammad/LogInActivity.java
+++ app/src/main/java/abd/askandr/mhmd/finallhayamuhammad/LogInActivity.java
@@ -2,12 +2,21 @@ package abd.askandr.mhmd.finallhayamuhammad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LogInActivity extends AppCompatActivity {
+    private EditText etName, etPassword;
+    private Button btnLogin, btnSignup, btnForget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        etName = (EditText) findViewById(R.id.etName);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnLogin= (Button) findViewById(R.id.btnLogin);
+        btnSignup = (Button) findViewById(R.id.btnSignup);
+        btnForget = (Button) findViewById(R.id.btnForget);
     }
 }
--- app/src/main/java/abd/askandr/mhmd/finallhayamuhammad/SignupActivity.java
+++ app/src/main/java/abd/askandr/mhmd/finallhayamuhammad/SignupActivity.java
@@ -2,12 +2,24 @@ package abd.askandr.mhmd.finallhayamuhammad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SignupActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity
+{
 
+    private EditText etName, etPass,etRePass, etMail;
+    private Button btnSave;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        etName = (EditText) findViewById(R.id.etName);
+        etPass = (EditText) findViewById(R.id.etPass);
+        etRePass = (EditText) findViewById(R.id.etRePass);
+        etMail = (EditText) findViewById(R.id.etMail);
+        btnSave = (Button) findViewById(R.id.btnSave);
     }
+
 }
